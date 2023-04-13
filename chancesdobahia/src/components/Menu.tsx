8b349026e5cdc36ed5c513580f0f64fe92d86d2a
rubensbrandas <rubens.brandas@gmail.com>
2023-04-13 10:38:32 -0300

    add menu

---
+++ chancesdobahia/src/components/Menu.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function Menu() {
+  const [show, setShow] = useState(false)
+
+  return (
+
+    <div className="menu">
+     <p>Menu</p>
+    </div>
+  )
+}
+
+export default Menu
