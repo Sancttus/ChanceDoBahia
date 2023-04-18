67f821b9dd5cba502abb755f992941af225052fa
rubensbrandas <rubens.brandas@gmail.com>
2023-04-18 11:40:08 -0300

    merge entre branchs

--- src/components/TabSelect.tsx
+++ src/components/TabSelect.tsx
@@ -8,7 +8,18 @@ import CompetitionComponent from "./Competition";
 import AchievementsComponent from "./achievements";
 import EnhancedTable from "./Team";
 import Divider from "@mui/material/Divider";
-
+import LastGame from "./Lastgame";
+import jacuipense from "../assets/jacuipense.png";
+import camboriu from "../assets/camboriu.png";
+import fluminense from "../assets/fluminense.png";
+import CRB from "../assets/CRB.png";
+import bahia from "../assets/bahia.svg";
+import vasco from "../assets/vasco.png";
+import santos from "../assets/santos.png";
+import curitiba from "../assets/curitiba.png";
+import bragantino from "../assets/red-bull-bragantino.svg";
+import botafogo from "../assets/botafogo.png";
+import voltaRedonda from "../assets/volta_redonda.png";
 interface TabPanelProps {
   children?: React.ReactNode;
   index: number;
@@ -65,35 +76,172 @@ export default function BasicTabs() {
       <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
         <Tabs value={value} onChange={handleChange} sx={{ fontFamily: "Play" }}>
           <Tab
+            sx={{ fontFamily: "Play" }}
+            label="Chances do Bahia"
+            {...a11yProps(0)}
+          />
+
+          <Tab
             sx={{ fontFamily: "Play", justifyContent: "center" }}
             label="Jogos"
-            {...a11yProps(0)}
+            {...a11yProps(1)}
           />
           <Tab
             sx={{ fontFamily: "Play" }}
             label="Competições"
-            {...a11yProps(1)}
+            {...a11yProps(2)}
           />
-          <Tab sx={{ fontFamily: "Play" }} label="Elenco" {...a11yProps(2)} />
+          <Tab sx={{ fontFamily: "Play" }} label="Elenco" {...a11yProps(3)} />
+
           <Tab
             sx={{ fontFamily: "Play" }}
             label="Conquistas"
-            {...a11yProps(3)}
+            {...a11yProps(4)}
           />
+          {/* <Tab
+            sx={{ fontFamily: "Play" }}
+            label="Histórico"
+            {...a11yProps(5)}
+          /> */}
         </Tabs>
       </Box>
       <TabPanel value={value} index={0}>
-        <MatchComponent
-          date="14/04/2023"
-          hour="18:30"
-          score1={2}
-          score2={0}
-          sigla1="ECB"
-          sigla2="RBR"
-          prediction={86}
-        />
+        <Box>
+          <MatchComponent
+            date="14/04/2023"
+            hour="18:30"
+            score1={2}
+            score2={0}
+            sigla1="ECB"
+            sigla2="RBR"
+            prediction={86}
+          />
+        </Box>
       </TabPanel>
       <TabPanel value={value} index={1}>
+        <Typography sx={{ color: "black" }}>Próximos Jogos</Typography>
+        <LastGame
+          competicao="Campeonato Brasileiro"
+          date="23-04-2023"
+          hours="20:00"
+          brasão1={bahia}
+          brasão2={botafogo}
+          sigla1="ECB"
+          sigla2="BTF"
+        />
+        <LastGame
+          competicao="Capa do Brasil"
+          date="27-04-2023"
+          hours="19:00"
+          brasão1={bahia}
+          brasão2={voltaRedonda}
+          sigla1="ECB"
+          sigla2="VRD"
+        />
+        <LastGame
+          competicao="Campeonato Brasileiro"
+          date="27-04-2023"
+          hours="19:00"
+          brasão1={vasco}
+          brasão2={bahia}
+          sigla1="VAS"
+          sigla2="ECB"
+        />
+        <LastGame
+          competicao="Campeonato Brasileiro"
+          date="27-04-2023"
+          hours="19:00"
+          brasão1={bahia}
+          brasão2={curitiba}
+          sigla1="ECB"
+          sigla2="CTB"
+        />
+        <LastGame
+          competicao="Campeonato Brasileiro"
+          date="27-04-2023"
+          hours="19:00"
+          brasão1={santos}
+          brasão2={bahia}
+          sigla1="ECS"
+          sigla2="ECB"
+        />
+        <LastGame
+          competicao="Campeonato Brasileiro"
+          date="24-04-2023"
+          hours="20:00"
+          brasão1={bahia}
+          brasão2={botafogo}
+          sigla1="ECB"
+          sigla2="BTF"
+        />
+        <LastGame
+          competicao="Capa do Brasil"
+          date="27-04-2023"
+          hours="19:00"
+          brasão1={bahia}
+          brasão2={voltaRedonda}
+          sigla1="ECB"
+          sigla2="VRD"
+        />
+
+        <Typography sx={{ color: "black" }}>últimos Jogos</Typography>
+        <LastGame
+          competicao="Campeonato Baiano"
+          date="15-04-2023"
+          hours="18:30"
+          brasão1={bragantino}
+          brasão2={bahia}
+          sigla1="RRB"
+          sigla2="ECB"
+          placar1={2}
+          placar2={1}
+        />
+        <LastGame
+          competicao="Campeonato Baiano"
+          date="02-04-2023"
+          hours="18:00"
+          brasão1={bahia}
+          brasão2={jacuipense}
+          sigla1="ECB"
+          sigla2="JAC"
+          placar1={3}
+          placar2={0}
+        />
+        <LastGame
+          competicao="Campeonato Baiano"
+          date="22-03-2023"
+          hours="21:30"
+          brasão1={bahia}
+          brasão2={CRB}
+          sigla1="ECB"
+          sigla2="CRB"
+          placar1={3}
+          placar2={0}
+        />
+        <LastGame
+          competicao="Campeonato Baiano"
+          date="14-03-2023"
+          hours="21:30"
+          brasão1={fluminense}
+          brasão2={bahia}
+          sigla1="ECB"
+          sigla2="FLU"
+          placar1={1}
+          placar2={1}
+        />
+        <LastGame
+          competicao="Campeonato Baiano"
+          date="22-03-2023"
+          hours="21:30"
+          brasão1={CRB}
+          brasão2={bahia}
+          sigla1="CRB"
+          sigla2="ECB"
+          placar1={1}
+          placar2={4}
+        />
+      </TabPanel>
+      <TabPanel value={value} index={2}>
         <Box
           sx={{
             gap: "2",
@@ -103,15 +251,15 @@ export default function BasicTabs() {
         >
           <CompetitionComponent
             titulo="Campeonato Brasileiro Série -A"
-            posicao={1}
+            posicao={18}
           />
           <Divider />
         </Box>
       </TabPanel>
-      <TabPanel value={value} index={2}>
+      <TabPanel value={value} index={3}>
         <EnhancedTable />
       </TabPanel>
-      <TabPanel value={value} index={3}>
+      <TabPanel value={value} index={4}>
         <Box>
           <AchievementsComponent />
         </Box>
